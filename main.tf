2cf433c980789d16192caf36ce0a2de34231927b
Maximilian Frank <maximilian.frank@ait.ac.at>
2020-02-14 12:19:04 +0100

    Update to new only var syntax

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "openstack_networking_secgroup_rule_v2" "mgmthost-rule-01" {
   port_range_min    = 22
   port_range_max    = 22
   remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = "${openstack_networking_secgroup_v2.mgmthost-sg.id}"
+  security_group_id = openstack_networking_secgroup_v2.mgmthost-sg.id
 }
 
 data "template_file" "user_data" {
@@ -40,8 +40,8 @@ data "template_cloudinit_config" "cloudinit" {
 #
 resource "openstack_compute_instance_v2" "mgmthost" {
   name               = "mgmthost"
-  flavor_name        = "${var.flavor}"
-  key_pair           = "${var.sshkey}"
+  flavor_name        = var.flavor
+  key_pair           = var.sshkey
 
   metadata = {
     groups = "management"
@@ -50,7 +50,7 @@ resource "openstack_compute_instance_v2" "mgmthost" {
   user_data = data.template_cloudinit_config.cloudinit.rendered
 
   block_device {
-    uuid                  = "${var.image_id}"
+    uuid                  = var.image_id
     source_type           = "image"
     volume_size           = 10
     boot_index            = 0
@@ -61,23 +61,23 @@ resource "openstack_compute_instance_v2" "mgmthost" {
   security_groups = ["mgmthost-sg"]
 
   network {
-     uuid = "${var.publicnet-id}"
+     uuid = var.publicnet-id
   }
 }
 
 
 resource "openstack_compute_interface_attach_v2" "net" {
-   count = "${length(var.network_ids)}"
-   instance_id = "${openstack_compute_instance_v2.mgmthost.id}"
-   network_id = "${element(var.network_ids,count.index)}"
+   count = length(var.network_ids)
+   instance_id = openstack_compute_instance_v2.mgmthost.id
+   network_id = element(var.network_ids,count.index)
 }
 
 resource "openstack_networking_floatingip_v2" "fip_1" {
-  pool = "${var.fip_pool}"
+  pool = var.fip_pool
 }
 
 resource "openstack_compute_floatingip_associate_v2" "fip_1" {
-  floating_ip = "${openstack_networking_floatingip_v2.fip_1.address}"
-  instance_id = "${openstack_compute_instance_v2.mgmthost.id}"
+  floating_ip = openstack_networking_floatingip_v2.fip_1.address
+  instance_id = openstack_compute_instance_v2.mgmthost.id
 }
 
